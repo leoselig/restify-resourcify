e2e4682c2f38ad11d11e0faaeee60336f5931378
leo.selig <dev.selig@gmail.com>
2016-01-14 00:28:12 +0100

    Make some methods ECMAScript2016+ async
    - add npm-debug.log to .gitignore

--- src/wrapHandler.js
+++ src/wrapHandler.js
@@ -1,17 +1,15 @@
 import {info as logInfo, error as logError} from 'npmlog';
 
 export default (handler) => {
-  return (request, response, next) => {
+  return async (request, response, next) => {
     logInfo('server', request.method + ' ' + request.url);
-    handler(request)
-    .then((responseInfo) => {
+    try {
+      const responseInfo = await handler(request);
       objectToResponse(responseInfo, response);
       next();
-    })
-    .catch((error) => {
+    } catch (error) {
       logError('server', error);
-    });
-    return handler.call(this, request);
+    }
   };
 };
 
