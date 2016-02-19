66574921d4f002fc7b660740a068608848360d27
leo.selig <dev.selig@gmail.com>
2016-02-19 15:33:06 +0100

    Remove chai-as-promised
    - no longer used (was added for the incorrect fix for silently caught
      errors)

--- test/setup.js
+++ test/setup.js
@@ -3,9 +3,7 @@ import 'source-map-support/register';
 import 'babel-polyfill';
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
-import chaiAsPromised from 'chai-as-promised';
 
 chai.use(sinonChai);
-chai.use(chaiAsPromised);
 
 npmlog.pause();
