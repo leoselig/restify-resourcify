761f4abd591afe8060439010b7dd73963b874b47
leo.selig <dev.selig@gmail.com>
2016-02-19 14:47:14 +0100

    Add chai-as-promised to test async functions
    - specifically, this is needed for exception testing in async functions
      which are hard to test with vanilla chai

--- test/setup.js
+++ test/setup.js
@@ -3,5 +3,9 @@ import 'source-map-support/register';
 import 'babel-polyfill';
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
+import chaiAsPromised from 'chai-as-promised';
+
 chai.use(sinonChai);
+chai.use(chaiAsPromised);
+
 npmlog.pause();
