439df891c7b6d919651abdf365f5a2d70bcfe694
leo.selig <dev.selig@gmail.com>
2016-04-11 19:10:47 +0200

    Add missing main export for @use()

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import registerResources from './register';
 import {HEAD, GET, POST, PUT, DELETE} from './decorators/http';
 import path from './decorators/path';
 import schema from './decorators/schema';
+import use from './decorators/use';
 
-export {HEAD, GET, POST, PUT, DELETE, path, schema, registerResources};
+export {
+  HEAD, GET, POST, PUT, DELETE,
+  path, schema, use,
+  registerResources
+};
